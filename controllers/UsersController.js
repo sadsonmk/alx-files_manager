c873f566f416a300b78b2fc1dca827711f66847c
Sadson Mk <sadsonmkumira@yahoo.com>
2024-05-11 08:20:27 +0000

    pushing 3. Create a new user files

---
+++ controllers/UsersController.js
@@ -0,0 +1,28 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+async function postNew(req, res) {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' });
+  }
+
+  if (!password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
+
+  const emailExists = await dbClient.client.db().collection('users').findOne({ email });
+
+  if (emailExists) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
+
+  const hashedPswd = sha1(password);
+  const newUser = await dbClient.client.db().collection('users').insertOne({ email, password: hashedPswd });
+  return res.status(201).json({ id: newUser.isertedId, email });
+}
+
+module.exports = {
+  postNew,
+};
