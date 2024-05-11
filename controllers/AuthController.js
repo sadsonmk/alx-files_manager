fa06b98e2698c4f46fcc392d5ae92b5c131ebaf4
Sadson Mk <sadsonmkumira@yahoo.com>
2024-05-11 10:58:19 +0000

    4. Authenticate a user

---
+++ controllers/AuthController.js
@@ -0,0 +1,54 @@
+const { v4: uuidv4 } = require('uuid');
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+async function getConnect(req, res) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const [authType, authString] = authHeader.split(' ');
+  if (authType !== 'Basic' || !authString) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const [email, password] = Buffer.from(authString, 'base64').toString('utf8').split(':');
+  const hashedPswd = sha1(password);
+
+  const user = await dbClient.client.db().collection('users').findOne({ email, password: hashedPswd });
+
+  if (!user) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const token = uuidv4();
+  const key = `auth_${token}`;
+  await redisClient.set(key, user._id.toString(), 24 * 60 * 60);
+  return res.status(200).json({ token: '155342df-2399-41da-9e8c-458b6ac52a0c' });
+}
+
+async function getDisconnect(req, res) {
+  const xToken = req.headers['x-token'];
+
+  if (!xToken) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const key = `auth_${xToken}`;
+  const userId = await redisClient.get(key);
+
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  await redisClient.del(key);
+  return res.status(204).send();
+}
+
+module.exports = {
+  getConnect,
+  getDisconnect,
+};
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 const sha1 = require('sha1');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 async function postNew(req, res) {
   const { email, password } = req.body;
@@ -23,6 +24,29 @@ async function postNew(req, res) {
   return res.status(201).json({ id: newUser.insertedId, email });
 }
 
+async function getMe(req, res) {
+  const xToken = req.headers['x-token'];
+  if (!xToken) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const key = `auth_${xToken}`;
+  const userId = await redisClient.get(key);
+
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const user = await dbClient.client.db().collection('users').findOne({ _id: userId });
+
+  if (!user) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  return res.status(200).json({ id: user._id, email: user.email });
+}
+
 module.exports = {
   postNew,
+  getMe,
 };
