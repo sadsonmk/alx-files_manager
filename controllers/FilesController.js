4639248549543bc237b78ae2c0f07b694e0515bc
Sadson Mk <sadsonmkumira@yahoo.com>
2024-05-14 10:19:36 +0000

    Adding files

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const uuidv4 = require('uuid').v4;
+const { ObjectId } = require('mongodb');
+const { promisify } = require('util');
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
+const writeFilePromise = promisify(fs.writeFile);
 const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
 
 async function postUpload(req, res) {
@@ -11,14 +14,13 @@ async function postUpload(req, res) {
   const {
     name, type, parentId = 0, isPublic = false, data,
   } = req.body;
-
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
   const key = `auth_${token}`;
   const userId = await redisClient.get(key);
-  const user = await dbClient.client.db().collection('users').findOne({ _id: userId });
+  const user = await dbClient.client.db().collection('users').findOne({ _id: new ObjectId(userId) });
   if (!user) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
@@ -27,7 +29,7 @@ async function postUpload(req, res) {
     return res.status(400).json({ error: 'Missing name' });
   }
 
-  if (!type || type !== 'folder' || type !== 'file' || type !== 'image') {
+  if (!type || !['folder', 'file', 'image'].includes(type)) {
     return res.status(400).json({ error: 'Missing type' });
   }
 
@@ -45,31 +47,31 @@ async function postUpload(req, res) {
     return res.status(400).json({ error: 'Missing data' });
   }
 
-  let localPath = null;
-
-  if (type === 'folder') {
-    const folderPath = FOLDER_PATH;
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath, { recursive: true });
-    }
-    const filename = uuidv4();
-    localPath = path.join(folderPath, filename);
-    fs.writeFileSync(localPath, data, 'base64');
-  }
-
   const newFile = {
     name,
     type,
     isPublic,
     parentId,
-    userId: userId.toStirng(),
+    userId,
   };
 
-  newFile.localPath = localPath;
-  const result = await dbClient.client.db().collection('files').insertOne(newFile);
-  newFile._id = result.insertedId;
+  if (type === 'folder') {
+    const result = await dbClient.client.db().collection('files').insertOne(newFile);
+    newFile.id = result.insertedId;
+    return res.status(201).json(newFile);
+  }
 
-  return res.status(201).json(newFile);
+  const filename = uuidv4();
+  const localPath = path.join(FOLDER_PATH, filename);
+  writeFilePromise(localPath, data, { encoding: 'base64' })
+    .then(async () => {
+      newFile.localPath = localPath;
+      const result = await dbClient.client.db().collection('files').insertOne(newFile);
+      newFile.id = result.insertedId;
+      return res.status(201).json(newFile);
+    })
+    .catch((err) => res.status(500).json({ error: err.messge }));
+  return res.status(201);
 }
 
 module.exports = {
