27bb19379cbe6f7048a07b822fd4ef959c9d6dd5
Sadson Mk <sadsonmkumira@yahoo.com>
2024-05-15 13:20:43 +0000

    Adding files

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,7 +4,7 @@ const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
 async function getConnect(req, res) {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.header('Authorization') || '';
 
   if (!authHeader) {
     return res.status(401).json({ error: 'Unauthorized' });
