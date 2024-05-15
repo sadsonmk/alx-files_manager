a77f31657fc781ddcc99c4fb948cb0c4a666c318
Sadson Mk <sadsonmkumira@yahoo.com>
2024-05-15 12:52:40 +0000

    Adding changes

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -20,8 +20,11 @@ async function postUpload(req, res) {
 
   const key = `auth_${token}`;
   const userId = await redisClient.get(key);
-  const user = await dbClient.client.db().collection('users').findOne({ _id: new ObjectId(userId) });
   if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  const user = await dbClient.client.db().collection('users').findOne({ _id: new ObjectId(userId) });
+  if (!user) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
 
