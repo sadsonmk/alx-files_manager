c2bf8e6b0b1462551b754c23361daf3b2e7230ae
Sadson Mk <sadsonmkumira@yahoo.com>
2024-05-15 18:56:14 +0000

    Adding files

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-const routes = require('./routes/index');
 const bodyParser = require('body-parser');
+const routes = require('./routes/index');
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
